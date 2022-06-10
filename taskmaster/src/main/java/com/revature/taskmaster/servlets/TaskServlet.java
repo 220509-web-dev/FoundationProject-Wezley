57aed905f2b34026265c6e7eea358aa7b89d1a54
Wezley Singleton <wezley.singleton@gmail.com>
2022-06-09 22:04:32 -0400

    task creation endpoint

--- taskmaster/src/main/java/com/revature/taskmaster/servlets/TaskServlet.java
+++ taskmaster/src/main/java/com/revature/taskmaster/servlets/TaskServlet.java
@@ -2,6 +2,8 @@ package com.revature.taskmaster.servlets;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.revature.taskmaster.dtos.ErrorResponse;
+import com.revature.taskmaster.dtos.NewTaskRequest;
+import com.revature.taskmaster.dtos.ResourceCreationResponse;
 import com.revature.taskmaster.entities.Task;
 import com.revature.taskmaster.exceptions.BadRequestException;
 import com.revature.taskmaster.exceptions.ResourceNotFoundException;
@@ -57,6 +59,29 @@ public class TaskServlet extends HttpServlet {
         resp.setStatus(501);
         resp.setHeader("Retry-After", LocalDateTime.now().plusDays(1).toString());
         resp.getWriter().write("{\"error\":\"No implementation for this operation yet\"}");
+
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+        System.out.println("[LOG] - TaskServlet#doPost received a request at " + LocalDateTime.now());
+        resp.setContentType("application/json");
+
+        try {
+            NewTaskRequest reqPayload = jsonMapper.readValue(req.getInputStream(), NewTaskRequest.class);
+            ResourceCreationResponse result = taskService.saveNewCard(reqPayload);
+            resp.setStatus(201);
+            resp.getWriter().write(jsonMapper.writeValueAsString(result));
+        } catch (BadRequestException e) {
+            resp.setStatus(400);
+            resp.getWriter().write(jsonMapper.writeValueAsString(new ErrorResponse(400, e.getMessage())));
+        } catch (Exception e) {
+            e.printStackTrace();
+            resp.setStatus(500);
+            ErrorResponse err = new ErrorResponse(500, "The server ran into an unhandled exception. Developers please check the server logs");
+            resp.getWriter().write(jsonMapper.writeValueAsString(err));
+        }
 
     }
 }
